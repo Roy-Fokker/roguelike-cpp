6cc1609685bb43c4151892ffd9d157ede279f0df
Neel Raiyani <nraiyani@gmail.com>
2019-10-06 16:39:44 -0400

    basic cmake application.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fmt/core.h>
+#include <chrono>
+#include <string>
+
+int main(int, char**) {
+    auto tp = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(tp);
+    fmt::print("Hello, World! {}", now_c);
+}
