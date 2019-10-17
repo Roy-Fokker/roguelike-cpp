c7cc86749788cb83e84fc39c22095e59a83db47e
Neel Raiyani <nraiyani@gmail.com>
2019-10-16 21:21:08 -0400

    Random Room Generator. Simple implementation

--- src/game_map.cpp
+++ src/game_map.cpp
@@ -1,10 +1,12 @@
 #include "game_map.hpp"
 
-#include <easy_iterator.h>
 #include <libtcod.hpp>
+#include <enumerate.hpp>
+#include <range.hpp>
+
 #include <random>
-#include <utility>
 #include <algorithm>
+#include <cassert>
 
 namespace
 {
@@ -13,175 +15,163 @@ namespace
 	static const auto tunnel_color = TCODColor(25, 25, 125);
 	static const auto wall_color = TCODColor(50, 50, 150);
 
-	constexpr auto max_rooms = 100;
-	constexpr auto min_room_size = 10;
-	constexpr auto max_room_size = 30;
+	constexpr auto max_rooms = 20;
+	constexpr auto min_room_size = 5;
+	constexpr auto max_room_size = 20;
 
-	struct rect
-	{
-		int x, y, w, h;
-
-		auto center() const -> std::pair<int, int>
-		{
-			return {
-				x + (w / 2),
-				y + (h / 2)
-			};
-		}
-
-		auto intersects(const rect &other) const -> bool
-		{
-			return (
-				x <= (other.x + other.w) and (x + w) >= other.x 
-				and
-				y <= (other.y + other.h) and (y + h) >= other.y
-			);
-		}
-	};
-
-	// struct to hold information about rooms.
-	// we need information as it pertains to our tiles array.
-	struct room
-	{
-		int start_idx;
-		int end_idx;
-		int idx_run;
-
-		rect r;
-	};
 
 	// Makes rooms of various sizes in the provided tile
 	// array. Ensures rooms don't overlap. 
 	// Returns array of rooms it created.
-	auto make_rooms(const map_size size, std::vector<tile> &tiles) -> std::vector<room>
+	auto make_rooms(const dimension size, std::vector<tile> &tiles) -> std::vector<room>
 	{
-		// Lambda that will take parameters to make room
-		// and update tiles array.
-		auto create_room = [&](rect r) -> room
+		// change the tiles to be of ground type. 
+		// assumes r is within bounds of size
+		auto change_tiles = [&](room r)
 		{
-			int w = std::min((r.x + r.w), size.width) - 1;  // Make sure we don't exceed width
-			int h = std::min((r.y + r.h), size.height) - 1; // and height. range is 0 to (dimension - 1)
-
-			auto start_idx = r.x + size.width * r.y;  // start index in tiles array
-			auto end_idx = w + size.width * h;    // end index in tiles array
-			auto idx_run = w - r.x;                 // length of each row of index
+			auto x1 = r.p.x,
+			     y1 = r.p.y;
+			auto x2 = r.p.x + r.size.width - 1,  // figure out x2, and y2 position
+			     y2 = r.p.y + r.size.height - 1; // range is 0 to (dimension - 1)
 			
-			for (auto i = start_idx; i <= end_idx; i++) // increment i to next index
+			for (auto y : iter::range(y1, y2))
 			{
-				tiles[i].blocked = false;         // unblock this tile
-				tiles[i].blocks_sight = false;
-				tiles[i].color = ground_color;
-
-				if (i >= start_idx + idx_run)     // check if index is within room's width
+				auto start_idx = x1 + size.width * y,
+				     end_idx = x2 + size.width * y;
+			
+				for (auto i : iter::range(start_idx, end_idx))
 				{
-					start_idx += size.width;      // go to next row.
-					i = start_idx - 1;            // make sure we set i to one minus start_idx
+					tiles[i].type = tile_type::ground;
 				}
 			}
-
-			return {(r.x + size.width * r.y), end_idx, idx_run, {r.x, r.y, w - r.x, h - r.y}};
 		};
 
-		auto make_random_rooms = [&]() -> std::vector<room>
+		std::random_device rd{};      // Random number generator
+		std::mt19937 gen(rd());       // generation algorithm we want to use
+		std::uniform_int_distribution d_rs(min_room_size, max_room_size), // distribution for room size
+		                              d_rx(0, size.width - min_room_size - 1), // x and y position of the room
+		                              d_ry(0, size.height - min_room_size - 1);// should be max size minus room size
+
+		// Makes a random room within our map size
+		auto make_random_room = [&]() -> room
 		{
-			std::random_device rd{};
-			std::mt19937 gen(rd());
-			std::uniform_int_distribution d_rs(min_room_size, max_room_size);
-			std::uniform_int_distribution d_rx(0, size.width - 1);
-			std::uniform_int_distribution d_ry(0, size.height - 1);
+			auto w = d_rs(gen),
+			     h = d_rs(gen),
+			     x = d_rx(gen),
+			     y = d_ry(gen);
+			
+			w = std::clamp(w + x, x, size.width - 1) - x; // clamp the output so 
+			h = std::clamp(h + y, y, size.height - 1) - y; // we don't exceed the map size
 
-			std::vector<room> rooms{};
-			for (auto i : easy_iterator::range(max_rooms))
-			{
-				// use random number generator to make a 
-				// room rectangle.
-				auto room_rect = rect
-				{
-					d_rx(gen),  // x and y coordinates
-					d_ry(gen),  // of the room.
-					d_rs(gen),  // width and height
-					d_rs(gen)   // of the room.
-				};
-
-				auto exists = std::find_if(std::begin(rooms), std::end(rooms),
-											[&](const auto &arg) {
-												return arg.r.intersects(room_rect);
-											});
-				if (exists == std::end(rooms))
-				{
-					rooms.push_back(create_room(room_rect));
-				}
-			}
-
-			return rooms;
+			return {x, y, w, h};
 		};
 
-		return make_random_rooms();
+		std::vector<room> rooms{};
 
+		while (rooms.size() < max_rooms)
+		{
+			auto room = make_random_room();  // Generate a room
+
+			// Check to see if this room intersects with any other room
+			auto exists = std::find_if(std::begin(rooms), std::end(rooms), [&](const auto &r)
+			{
+				return room.intersects(r);
+			});
+			
+			// if it does not intersect, then add it to our list
+			if (exists == std::end(rooms))
+			{
+				rooms.push_back(room);
+			}
+		}
+
+		// Update the tiles for all the 
+		// room we have so far.
+		for (auto &r : rooms)
+		{
+			change_tiles(r);
+		}
+
+		return rooms;
+
+		// change_tiles({20, 15, 10, 15});
+		// change_tiles({35, 15, 10, 15});
+		// change_tiles({10, 10, 5, 5});
+		
 		// return {
-		// 	create_room({20, 15, 10, 15}),
-		// 	create_room({35, 15, 10, 15}),
-		// 	create_room({10, 10, 5, 5})
+		// 	{20, 15, 10, 15},
+		// 	{35, 15, 10, 15},
+		// 	{10, 10, 5, 5},
 		// };
 	}
 
 	// Connect all the rooms in order they got added to the list
-	void connect_rooms(const map_size size, const std::vector<room> &rooms, std::vector<tile> &tiles)
+	void connect_rooms(const dimension size, const std::vector<room> &rooms, std::vector<tile> &tiles)
 	{
-		auto create_tunnel = [&](const room &r1, const room &r2)
-		{
-			auto [x1, y1] = r1.r.center();
-			auto [x2, y2] = r2.r.center();
 
-			auto dig_tunnel = [&](int idx)
-			{
-				if (not tiles[idx].blocked)
-					return;
+	}
 
-				tiles[idx].blocked = false;         // unblock this tile
-				tiles[idx].blocks_sight = false;
-				tiles[idx].color = tunnel_color;
-			};
 
-			auto horizontal_tunnel = [&](bool min_max)
-			{
-				auto x_start = std::min(x1, x2);
-				auto x_end = std::max(x1, x2);
-				auto y = (min_max) ? std::min(y1, y2) : std::max(y1, y2);
+	// // Connect all the rooms in order they got added to the list
+	// void connect_rooms(const map_size size, const std::vector<room> &rooms, std::vector<tile> &tiles)
+	// {
+	// 	auto create_tunnel = [&](const room &r1, const room &r2)
+	// 	{
+	// 		auto [x1, y1] = r1.r.center();
+	// 		auto [x2, y2] = r2.r.center();
 
-				for (int x = x_start; x < x_end; x++)
-				{
-					auto idx = x + size.width * y;
-					dig_tunnel(idx);
-				}
-			};
+	// 		auto dig_tunnel = [&](int idx)
+	// 		{
+	// 			if (not tiles[idx].blocked)
+	// 				return;
 
-			auto vertical_tunnel = [&](bool min_max)
-			{
-				auto y_start = std::min(y1, y2);
-				auto y_end = std::max(y1, y2);
-				auto x = (min_max) ? std::min(x1, x2) : std::max(x1, x2);
+	// 			tiles[idx].blocked = false;         // unblock this tile
+	// 			tiles[idx].blocks_sight = false;
+	// 			tiles[idx].color = tunnel_color;
+	// 		};
 
-				for (int y = y_start; y < y_end; y++)
-				{
-					auto idx = x + size.width * y;
-					dig_tunnel(idx);
-				}
-			};
+	// 		auto horizontal_tunnel = [&](bool min_max)
+	// 		{
+	// 			auto x_start = std::min(x1, x2);
+	// 			auto x_end = std::max(x1, x2);
+	// 			auto y = (min_max) ? std::min(y1, y2) : std::max(y1, y2);
+
+	// 			for (int x = x_start; x < x_end; x++)
+	// 			{
+	// 				auto idx = x + size.width * y;
+	// 				dig_tunnel(idx);
+	// 			}
+	// 		};
+
+	// 		auto vertical_tunnel = [&](bool min_max)
+	// 		{
+	// 			auto y_start = std::min(y1, y2);
+	// 			auto y_end = std::max(y1, y2);
+	// 			auto x = (min_max) ? std::min(x1, x2) : std::max(x1, x2);
+
+	// 			for (int y = y_start; y < y_end; y++)
+	// 			{
+	// 				auto idx = x + size.width * y;
+	// 				dig_tunnel(idx);
+	// 			}
+	// 		};
 			
-			std::random_device rd{};
-			std::mt19937 gen(rd());
-			std::bernoulli_distribution d(0.5);
+	// 		std::random_device rd{};
+	// 		std::mt19937 gen(rd());
+	// 		std::bernoulli_distribution d(0.5);
 			
-			auto min_max = d(gen);
-			horizontal_tunnel(min_max);
-			vertical_tunnel(not min_max);
-		};
+	// 		auto min_max = d(gen);
+	// 		horizontal_tunnel(min_max);
+	// 		vertical_tunnel(not min_max);
+	// 	};
 
-		for (int i = 1; i < rooms.size(); i++)
-		{
-			create_tunnel(rooms[i-1], rooms[i]);
-		}
+	// 	for (int i = 1; i < rooms.size(); i++)
+	// 	{
+	// 		create_tunnel(rooms[i-1], rooms[i]);
+	// 	}
+	// }
+}
+
 auto tile::is_blocked() const -> bool
 {
 	if (type == tile_type::wall)
