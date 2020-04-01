c73c9a81bf64b194a0b62b13d595728ad88012e7
Neel Raiyani <nraiyani@gmail.com>
2020-04-01 14:17:20 -0400

    formatting and change true/false to on/off.

--- cmake/msvc_settings.cmake
+++ cmake/msvc_settings.cmake
@@ -1,8 +1,8 @@
 # Setup configuration specific to MSVC
 
 function(set_msvc_project_configuration project_name)
-    option(MSVC_CXX_17_MODE "Enable C++17" TRUE)
-    option(MSVC_CXX_LATEST  "Enable C++Latest" FALSE)
+    option(MSVC_CXX_17_MODE "Enable C++17" ON)
+    option(MSVC_CXX_LATEST  "Enable C++Latest" OFF)
 
     set(MSVC_COMPILER_FLAGS
             /W3             # Enable all warnings
@@ -21,10 +21,12 @@ function(set_msvc_project_configuration project_name)
             WIN32_LEAN_AND_MEAN # Trim it down.
     )
 
+    # if both options are true, only 17 mode is enabled.
+    # if neither option is enabled, MSVC will use it's default.
     if(MSVC_CXX_17_MODE)
         set(MSVC_COMPILER_FLAGS 
                 ${MSVC_COMPILER_FLAGS}
-                /std:c++17  # use C++ 17 
+                /std:c++17      # use C++ 17 
         )
     elseif(MSVC_CXX_LATEST)
         set(MSVC_COMPILER_FLAGS 
@@ -33,8 +35,10 @@ function(set_msvc_project_configuration project_name)
         )
     endif()
 
+    # Only enable if current compiler is MSVC.
     if (MSVC)
         message("Enable MSVC configuraiton.")
+
         target_compile_options(${project_name}
             INTERFACE
                 ${MSVC_COMPILER_FLAGS}
